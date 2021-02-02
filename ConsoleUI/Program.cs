2c2e9fa14e1fe0c31951c4ef0037261374ab05a7
Melih Boz <melihbooz@gmail.com>
2021-02-02 03:02:24 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,25 @@
+﻿
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine("Araba id: "+ car.CarId);
+                Console.WriteLine("Araba model id: "+car.BrandId);
+                Console.WriteLine("Araba model id: "+car.ColorId);
+                Console.WriteLine("Araba model yılı: "+car.ModelYear);
+                Console.WriteLine("Araba günlük kiralama ücreti: "+car.DailyPrice);
+                Console.WriteLine("Tanım: "+car.Description);
+                Console.WriteLine("------------");
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+        public InMemoryCarDal()
+        {
+            _cars = new List<Car> 
+            {
+                new Car{CarId=1,BrandId=1,ColorId=323,DailyPrice=100,ModelYear=2016,Description="manuel vites sedan"},
+                new Car{CarId=2,BrandId=5,ColorId=435,DailyPrice=120,ModelYear=2018,Description="otomatik vites station vagon"},
+                new Car{CarId=3,BrandId=3,ColorId=231,DailyPrice=150,ModelYear=2019,Description="otomatik vites hatchback"},
+                new Car{CarId=4,BrandId=7,ColorId=764,DailyPrice=90,ModelYear=2013,Description="otomatik vites sedan"},
+                new Car{CarId=5,BrandId=13,ColorId=964,DailyPrice=500,ModelYear=2021,Description="manuel vites hatchback"}
+
+            };
+        }
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(p => p.CarId == car.CarId);
+            _cars.Remove(carToDelete);
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetById(int carId)
+        {
+            return _cars.Where(p=>p.CarId==carId).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(p => p.CarId == car.CarId);
+
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            
+        }
+    }
+}
