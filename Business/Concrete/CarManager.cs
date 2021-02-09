1c816d1d4aba7554c4cdb8f6760d91a15087907d
Melih Boz <melihbooz@gmail.com>
2021-02-10 01:11:39 +0300

    10.02.2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -2,6 +2,7 @@
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -44,6 +45,11 @@ namespace Business.Concrete
         public Car GetById(int id)
         {
             return _carDal.GetById(p=>p.CarId==id);
+        }
+
+        public List<CarDetailDto> GetCarDetails()
+        {
+            return _carDal.GetCarDetails();
         }
 
         public void Update(Car car)
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,27 +11,31 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            CarManager carManager = new CarManager(new EfCarDal());
-            BrandManager brandManager = new BrandManager(new EfBrandDal());
-            ColorManager colorManager = new ColorManager(new EfColorDal());
+            //BrandManager brandManager = new BrandManager(new EfBrandDal());
+            //ColorManager colorManager = new ColorManager(new EfColorDal());
 
+
+
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine("-{0} marka, {1} renk, {2} Tl, {3} model, {4}\n", brandManager.GetById(car.BrandId).BrandName, colorManager.GetById(car.ColorId).ColorName, car.DailyPrice, car.ModelYear, car.Description);
+            //}
+
+
+            //brandManager.Add(new Brand { BrandName="m"});
 
             
-            foreach (var car in carManager.GetAll())
+
+            //Console.WriteLine("Eklemeden sonraki yeni tablo:\n");
+            GetCarDetailsTest();
+        }
+
+        private static void GetCarDetailsTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetCarDetails())
             {
-                Console.WriteLine("-{0} marka, {1} renk, {2} Tl, {3} model, {4}\n", brandManager.GetById(car.BrandId).BrandName, colorManager.GetById(car.ColorId).ColorName, car.DailyPrice, car.ModelYear, car.Description);
-            }
-
-
-            brandManager.Add(new Brand { BrandName="m"});
-
-            brandManager.Add(new Brand {BrandName="Toyota" });
-
-            Console.WriteLine("Eklemeden sonraki yeni tablo:\n");
-
-            foreach (var brand in brandManager.GetAll())
-            {
-                Console.WriteLine(brand.BrandName);
+                Console.WriteLine(car.BrandName + " -- " + car.Description + " -- " + car.ColorName + " -- " + car.DailyPrice);
             }
         }
     }
